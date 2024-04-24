96ff1cbcbb066e475895b2da9f19ec1cd8e7d1cf
Krishna Patel <157887090+krishnapatel36@users.noreply.github.com>
2024-04-24 23:36:48 +0530

    Update new_hexa.py

--- new_hexa.py
+++ new_hexa.py
@@ -1,12 +1,13 @@
 import streamlit as st
+from pentominoes import ALL_PARTS
 from polymino import Grid,generate_polyminoes,generate_polymino_positions,unique_grids
 from dlx import DLX
-from hexa_rotation_flip import P1,P2,P3,P4,P5,P6,P7,P8
+from hexa_rotation_flip import P1,P2,P3,P4,P5,P6,P7,P8,P9,P10
 from itertools import product
 from svgwrite import Drawing,text
 import numpy as np  
 
-st.title("षट्कोण पहेलि")
+st.title("Shatkon Paheli")
 
 def solutions_svg(solutions, filename, columns=1, size=5, padding=10,
                   colour=lambda _: "white",stroke_colour="black",
@@ -58,7 +59,7 @@ def solutions_svg(solutions, filename, columns=1, size=5, padding=10,
                 piece.add(drawing.polygon(points))
 
 
-        # # put in "empty" pieces
+        # put in "empty" pieces
         num=1
         for i, j in solution.coord:
             if grid[i][j] == empty:
@@ -93,97 +94,87 @@ def solutions_svg(solutions, filename, columns=1, size=5, padding=10,
     drawing.save()
 
 number = st.selectbox("Select a Date", options=list(range(1, 32)), index=0)
-if number==26:
-    st.write("OOPS!! NO SOLUTION FOUND")
-else:
-    date_map={
-        1:(0,2),
-        2:(0,3),
-        3:(0,4),
-        4:(0,5),
-        5:(0,6),
-        6:(0,7),
-        7:(1,2),
-        8:(1,3),
-        9:(1,4),
-        10:(1,5),
-        11:(1,6),
-        12:(1,7),
-        13:(2,1),
-        14:(2,2),
-        15:(2,3),
-        16:(2,4),
-        17:(2,5),
-        18:(2,6),
-        19:(2,7),
-        20:(3,1),
-        21:(3,2),
-        22:(3,3),
-        23:(3,4),
-        24:(3,5),
-        25:(3,6),
-        26:(4,0),
-        27:(4,1),
-        28:(4,2),
-        29:(4,3),
-        30:(4,4),
-        31:(4,5)
-    }
-    date_row_col=date_map[number]
+date_map={
+    1:(0,2),
+    2:(0,3),
+    3:(0,4),
+    4:(0,5),
+    5:(0,6),
+    6:(0,7),
+    7:(1,1),
+    8:(1,2),
+    9:(1,3),
+    10:(1,4),
+    11:(1,5),
+    12:(1,6),
+    13:(1,7),
+    14:(2,0),
+    15:(2,1),
+    16:(2,2),
+    17:(2,4),
+    18:(2,5),
+    19:(2,6),
+    20:(3,0),
+    21:(3,1),
+    22:(3,2),
+    23:(3,3),
+    24:(3,4),
+    25:(3,5),
+    26:(4,0),
+    27:(4,1),
+    28:(4,2),
+    29:(4,3),
+    30:(4,4),
+    31:(4,5)
+}
+date_row_col=date_map[number]
 
-    def sortkey(x):
-        x = str(x)
-        return (len(x), x)
+def sortkey(x):
+    x = str(x)
+    return (len(x), x)
 
-    COLOURS = dict(I="#f15bb5", F="#9b5de5", L="#00bbf9",
-                P="#c7ffff", N="#7aff60", T="#e8fa42",
-                U="#fb8f23", V="#f44a4a", W="#AAAAEE",
-                X="#BB99DD", Y="#CC88CC", Z="#DD99BB")
-    GRID = Grid((5, 8), holes=[(0, 0), (1, 0), (2, 0), (3, 0), (0,1), (1,1), (4,6), (3,7), (4,7),date_row_col])
-    print(GRID)
-    main_polyminoes=[]
+COLOURS = dict(I="#f15bb5", F="#9b5de5", L="#00bbf9",
+            P="#c7ffff", N="#7aff60", T="#e8fa42",
+            U="#fb8f23", V="#f44a4a", W="#AAAAEE",
+            X="#BB99DD", Y="#CC88CC", Z="#DD99BB")
+GRID = Grid((5, 8), holes=[(0,0),(0,1),(1,0),(2,3),(2,7),(3,6),(3,7),(4,7),(4,6),date_row_col])
+print(GRID)
+main_polyminoes=[]
 
-    pieces=[P1,P2,P3,P4,P5,P6,P7,P8]
-    shade=['I','F','L','P','N','T','U','V','W','X','Y','Z']
+pieces=[P1,P2,P3,P4,P5,P6,P7,P8]
+shade=['I','F','L','P','N','T','U','V','W','X','Y','Z']
 
-    all_solutions = []  
-    j=0
+all_solutions = []  
+j=0
 
-    for SHAPES in pieces:
-        polyminoes=[]
-        for piece in generate_polyminoes(SHAPES):
-            for position in generate_polymino_positions(piece, GRID):
-                if position not in polyminoes:
-                    polyminoes.append(position)
-        polyminoes = [polymino.aslist for polymino in polyminoes]
-        polyminoes = [list(reversed(inner_list)) for inner_list in polyminoes]
-        for i in polyminoes:
-            i.pop()
-            i.append(shade[j])
-        j+=1
-        polyminoes = [list(reversed(inner_list)) for inner_list in polyminoes]
-        # print("polyminoes",polyminoes)
-        main_polyminoes+=polyminoes
+for SHAPES in pieces:
+    polyminoes=[]
+    for piece in generate_polyminoes(SHAPES):
+        for position in generate_polymino_positions(piece, GRID):
+            if position not in polyminoes:
+                polyminoes.append(position)
+    polyminoes = [polymino.aslist for polymino in polyminoes]
+    polyminoes = [list(reversed(inner_list)) for inner_list in polyminoes]
+    for i in polyminoes:
+        i.pop()
+        i.append(shade[j])
+    j+=1
+    polyminoes = [list(reversed(inner_list)) for inner_list in polyminoes]
+    main_polyminoes+=polyminoes
 
-    LABELS = list(set([element for polymino in main_polyminoes for element in polymino]))
-    LABELS = sorted(LABELS, key=sortkey)
+LABELS = list(set([element for polymino in main_polyminoes for element in polymino]))
+LABELS = sorted(LABELS, key=sortkey)
 
-    # print("Grid\n\n",GRID)
-    # print("labels\n\n",LABELS)
-    # print("poly\n\n",main_polyminoes)
+COVER = DLX(LABELS, main_polyminoes)
+for i, SOLUTION in enumerate(COVER.generate_solutions()):
+    solution_grid = Grid.from_DLX(SOLUTION)
+    all_solutions.append(solution_grid)
+    break
 
-    COVER = DLX(LABELS, main_polyminoes)
-    for i, SOLUTION in enumerate(COVER.generate_solutions()):
-        solution_grid = Grid.from_DLX(SOLUTION)
-        # print(solution_grid)
-        all_solutions.append(solution_grid)
-        break
-    # print(all_solutions)
-
-    solutions_svg([all_solutions[0]], filename='first_solution.svg', columns=7,colour=COLOURS.get)
-    svg_content = open("first_solution.svg", "r").read()
-    st.write(f"solution for DATE: {number}")
-    st.image(svg_content, width=2000)
+solutions_svg([all_solutions[0]], filename='first_solution.svg', columns=7,colour=COLOURS.get)
+svg_content = open("first_solution.svg", "r").read()
+st.write(f"solution for DATE: {number}")
+st.image(svg_content, width=2000)
 
 
 
